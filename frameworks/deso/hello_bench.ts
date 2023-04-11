9de9007b3f36349e6629ec44e8336a0e85cc8a7b
Gautham Ramachandran <gautham.ramachandran@shopify.com>
2023-04-11 17:03:07 +0200

    Trigger pipeline

--- frameworks/deso/hello_bench.ts
+++ frameworks/deso/hello_bench.ts
@@ -2,6 +2,6 @@ import { Deso } from "https://deno.land/x/deso/mod.ts";
 
 const app = new Deso();
 
-app.get("/", (context) => context.text("Hello, Bench!"));
+app.get("/", (context) => context.text("Hello, Bench!!"));
 
 await app.serve({ port: 8000 });
